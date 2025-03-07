a98b97bea52fa0b86f90cf045167d4ecf7b0bf48
Estevao Costa <tevito220303@gmail.com>
2025-03-06 23:30:02 -0300

    adicionando pasta atividades e um novo exercício

---
+++ atividades/ex02.js
@@ -0,0 +1,24 @@
+// Questão 01
+let nome = "Estevão Costa";
+
+let mai = nome.toUpperCase();
+console.log("Maiúsculas:", mai);
+
+let min = nome.toLocaleLowerCase();
+console.log("Minúsculas:", min)
+
+// Questão 02
+
+// Questão 03
+
+// Questão 04
+
+// Questão 05
+
+// Questão 06
+
+// Questão 07
+
+// Questão 08
+
+// Questão 09
